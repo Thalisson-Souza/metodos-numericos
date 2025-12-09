9230d3511283fc8d447ccb79b77df21248a54d1a
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-12-09 13:03:25 -0300

    fix: f`(xn) != 0

--- implementations/root-finding/metodoNewtonRaphson.c
+++ implementations/root-finding/metodoNewtonRaphson.c
@@ -3,33 +3,38 @@
 #include <math.h>
 
 double f(double x){
-    return (x * x ) - 2;
+    return sin(x) - exp(x);
 }
 
 double f_derivada(double x){
-    return 2 * x;
+    return cos(x) - exp(x);
 }
 
 int main(){
     double epsilon = 0.001;
     double xn[100];
     int i = 0;
-    
-    xn[i] = 2;
-
     double fXn;
+    
+    xn[i] = 10;
 
     do{
+        double derivada = f_derivada(xn[i]);
+
+        if(fabs(derivada) < 0.000001){
+            printf("deu ruim aqui -> f linha(%.6f) = 0\n", xn[i]);
+            return 0;
+        }
+
         i++;
-        xn[i] = xn[i - 1] -  f(xn[i - 1]) / f_derivada(xn[i - 1]);
-        printf("iteracao %d: x%d = %.5f\n", i, i, xn[i]);
-
+        xn[i] = xn[i - 1] -  f(xn[i - 1]) / derivada;
         fXn = f(xn[i]);
-        printf("%.6f\n", fXn);
-        
-    }while( (fabs(fXn) > epsilon )  ||  ( ( fabs(xn[i]) )  - ( fabs(xn[0]) )   >  epsilon ));
 
-    printf("\nResulta na raiz aproximada: %.4f\n", xn[i]);
+        printf("iteracao %d: x%d = %.6f  ---  f(x%d) = %.6f\n", i, i, xn[i], i, fXn);
+        
+    }while(fabs(f(xn[i])) > epsilon || fabs(xn[i] - xn[i-1]) > epsilon);
+
+    printf("\nResulta na raiz aproximada: %.6f\n", xn[i]);
 
 }
 
