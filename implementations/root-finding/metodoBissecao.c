36ecca1b003b5a76c7dddb3d673e0972ab6f8ead
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-12-09 05:05:27 -0300

    fix: att intervalo & verificar bolzano

--- implementations/root-finding/metodoBissecao.c
+++ implementations/root-finding/metodoBissecao.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <math.h>
 
-double fX(double x){
+double f(double x){
     return (x * x) - 2;
 }
 
@@ -10,44 +10,51 @@ double pontoMedio(double a, double b){
     return (a + b) / 2;
 }
 
+bool bolzano(double a, double b){
+    return f(a) * f(b) < 0;
+}
 
 int main(){
     double a = 0;
     double b = 3;
     double tolerancia = 0.05;
-    
-    double fA;
-    double fB;
-    double fPm;
-    double pontoMedioAtual;
+
+    if(!bolzano(a, b)){
+        printf("intervalo deu ruim ai!, f(a) e f(b) tem mesmo sinal!");
+        return 0;
+    }
+
+    double fA, fB, fPm, pontoMedioAtual;
+    int interacoes = 0;
 
     printf("Inicio: a = %.0f  b = %.0f\n", a, b);
-    pontoMedioAtual = pontoMedio(a, b);
 
     do{
-        fA = fX(a);
-        fB = fX(b);
+        interacoes++;
+        fA = f(a);
+        fB = f(b);
 
-        printf("\n Nova Iteracao \n");
+        pontoMedioAtual = pontoMedio(a, b);
+        fPm = f(pontoMedioAtual);
+
+        printf("\n Nova Iteracao\n");
         printf("a = %.0f  b = %.0f  fA = %.2f  fB = %.2f\n", a, b, fA, fB);
 
-        if(fA * fB < 0){
-            pontoMedioAtual = pontoMedio(a,b);
-            fPm = fX(pontoMedioAtual);
-
-            printf("pontoMedioAtual = %.2f  fPm = %.2f\n", pontoMedioAtual, fPm);
-            printf("fX(pontoMedioAtual) = %.2f\n", fPm);
-
-            if(fabs(fPm <= tolerancia)){
-                printf("Raiz aproximada encontrada: %.2f\n", pontoMedioAtual);
-                break; 
-            } else if(fA * fPm < 0){
-                printf("Escolhendo subintervalo: [a, Pm]\n");
-                b = pontoMedioAtual;
-            } else{
-                printf("Escolhendo subintervalo: [Pm, b]\n");
-                a = pontoMedioAtual;
-            }
+        if(fabs(fPm) <= tolerancia){
+            printf("\nConvergencia pela IMAGEM -> raiz aproximada: %.4f\n", pontoMedioAtual);
+            break;
         }
-    }while(fabs(fX(pontoMedioAtual)) > tolerancia);
+
+        if(fA * fPm < 0){
+            b = pontoMedioAtual; // raiz está em [a, m]
+        } else {
+            a = pontoMedioAtual; // raiz está em [m, b]
+        }
+
+        if(fabs(b - a) <= tolerancia){
+            printf("\nConvergencia pelo DOMINIO -> raiz aproximada: %.4f\n", pontoMedioAtual);
+            break;
+        }
+
+    }while(true);
 }
